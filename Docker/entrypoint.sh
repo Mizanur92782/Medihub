878651fde4c82d9139ce7ef6ba2b8719c64f6e95
Mizanur Rahman <nightliver000@gmail.com>
2026-05-05 13:19:48 +0600

    Docker, nginx production setup

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# Only run migrate and collectstatic on first replica
+if [ "$REPLICA_ID" = "1" ] || [ -z "$REPLICA_ID" ]; then
+  python manage.py migrate --noinput
+  python manage.py collectstatic --noinput
+fi
 
+if [ "$DJANGO_DEV" = "true" ]; then
+  exec python manage.py runserver 0.0.0.0:8011
+else
+  exec gunicorn medihub.wsgi:application \
+    --bind 0.0.0.0:8011 \
+    --workers 4 \
+    --threads 2 \
+    --timeout 60 \
+    --access-logfile - \
+    --error-logfile -
+fi
