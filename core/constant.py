ed839db2e2254c1fb104789a4a32c496bfebf16e
Mizanur Rahman <nightliver000@gmail.com>
2026-06-02 21:36:56 +0600

    fix profile

--- core/constant.py
+++ core/constant.py
@@ -4,10 +4,11 @@ from profiles.models.blood_donor_mod import BloodDonor
 from profiles.models.diagnostic_prof_mod import DiagnosticProfile
 from profiles.models.doctor_prof_mod import Doctor
 from profiles.models.pharmacy_prof_mod import PharmacyProfile
+from profiles.models.user_prof_mod import RegularUserProfile
 
 
 PROFILE_MAP = {
-    RoleChoices.REGULAR: RegularUserProfile, # pyright: ignore[reportUndefinedVariable]
+    RoleChoices.REGULAR: RegularUserProfile,
     RoleChoices.DOCTOR: Doctor,
     RoleChoices.AMBULANCE: AmbulanceProfile,
     RoleChoices.PHARMACY: PharmacyProfile,
